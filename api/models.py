3fd745f90584ed46a5b77982ced3c17d45831cd1
nmaslov255 <nmaslov255@yandex.ru>
2023-05-11 04:57:19 +0300

    created Package & PackageType models (without validations)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class PackageType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Package(models.Model):
+    name = models.CharField(max_length=255)
+    weight = models.FloatField()
+    price = models.PositiveIntegerField()
+    package_type = models.ForeignKey(models, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
