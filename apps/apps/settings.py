a74f98a588941eb43ae50525932af7e06f3c84d5
nmaslov255 <nmaslov255@yandex.ru>
2023-05-17 15:21:28 +0300

    upd parser for getenv(DJANGO_DEBUG)

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(getenv('DJANGO_DEBUG'))
+DEBUG = getenv('DJANGO_DEBUG') in ('1', 'true')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
